a87120911897355434e77cff329746c5354bfb87
noah <qkrwldn0818@naver.com>
2025-11-25 13:24:27 +0900

    chore: 디렉토리 구조 세팅

---
+++ src/main/java/com/pagoda/matchmeal/MatchmealApplication.java
@@ -0,0 +1,13 @@
+package com.pagoda.matchmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchmealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pagoda/matchmeal/MatchmealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagoda.matchmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
