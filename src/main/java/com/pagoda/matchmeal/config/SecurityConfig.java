2e8b8917d192d1cfcb9b8bee37e12c5e2215e1c1
seho1278 <seho1278@naver.com>
2025-11-30 04:28:21 +0900

    SecurityConfig 수정

--- src/main/java/com/pagoda/matchmeal/config/SecurityConfig.java
+++ src/main/java/com/pagoda/matchmeal/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.pagoda.matchmeal.config;
 
+import com.pagoda.matchmeal.config.oauth.OAuth2SuccessHandler;
+import com.pagoda.matchmeal.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/**").permitAll()
+                // CSRF, HTTP Basic 비활성화
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(basic -> basic.disable())
+                // 세션 사용 x
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // URL 권한 설정
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/login/**").permitAll()
                         .anyRequest().authenticated()
+                )
+
+                // OAuth2 로그인 설정
+                .oauth2Login(oauth2 -> oauth2
+                        // 로그인 성공
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        // 성공 후 핸들러
+                        .successHandler(oAuth2SuccessHandler)
                 );
 
         return http.build();
