17d38ea28de6d01e8f9101d0ceb8aefd004ebd1d
seho1278 <seho1278@naver.com>
2025-11-30 03:27:41 +0900

    UserMapper 설정

---
+++ src/main/java/com/pagoda/matchmeal/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.pagoda.matchmeal.mapper;
+
+import com.pagoda.matchmeal.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
+public interface UserMapper {
+    // 회원 가입
+    void save(User user);
+
+    // 조회(소셜ID)
+    Optional<User> findBySocialId(String socialId);
+
+
+}
