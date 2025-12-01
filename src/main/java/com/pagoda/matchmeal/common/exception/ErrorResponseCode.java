025cd8184eb538fa6a9d4f556e0c83c01ac7e31e
noah <qkrwldn0818@naver.com>
2025-12-02 01:27:47 +0900

    feat(food): 권한 및 음식 에러코드 생성

--- src/main/java/com/pagoda/matchmeal/common/exception/ErrorResponseCode.java
+++ src/main/java/com/pagoda/matchmeal/common/exception/ErrorResponseCode.java
@@ -9,7 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorResponseCode {
 
     //----------------------------공통코드----------------------------
-    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다."),
+
+    //----------------------------음식DB 에러코드----------------------------
+    FOOD_NOT_FOUND(HttpStatus.NOT_FOUND, "음식 데이터를 찾을 수 없습니다.");
+
 
     private final HttpStatus status; // 에러 HTTP 상태 코드
     private final String message; // 에러 메세지
