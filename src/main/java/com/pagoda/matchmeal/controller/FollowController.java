0fdbac34b89dc1dfbc99c0bfaceaba517b271f93
seho1278 <seho1278@naver.com>
2025-12-10 02:37:08 +0900

    feat: follow list 조회 controller ì 작성

--- src/main/java/com/pagoda/matchmeal/controller/FollowController.java
+++ src/main/java/com/pagoda/matchmeal/controller/FollowController.java
@@ -1,15 +1,15 @@
 package com.pagoda.matchmeal.controller;
 
 import com.pagoda.matchmeal.model.dto.UserDto;
+import com.pagoda.matchmeal.model.dto.response.FollowListDto;
 import com.pagoda.matchmeal.model.dto.response.FollowResponseDto;
 import com.pagoda.matchmeal.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -33,5 +33,35 @@ public class FollowController {
         }
         FollowResponseDto response = followService.toggleFollow(userDto.getId(), targetId);
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 팔로워 목록 조회
+     * @param userDto
+     * @param userId
+     */
+    @GetMapping("/{userId}/followers")
+    public ResponseEntity<List<FollowListDto>> getFollowers(
+            @AuthenticationPrincipal UserDto userDto, // 조회하는 사람
+            @PathVariable Long userId
+    ) {
+        Long viewerId = (userDto != null) ? userDto.getId() : null;
+        List<FollowListDto> list = followService.getFollowers(userId, viewerId);
+        return ResponseEntity.ok(list);
+    }
+
+    /**
+     * 팔로잉 목록 조회
+     * @param userDto
+     * @param userId
+     */
+    @GetMapping("/{userId}/followings")
+    public ResponseEntity<List<FollowListDto>> getFollowings(
+            @AuthenticationPrincipal UserDto userDto, // 조회하는 사람
+            @PathVariable Long userId
+    ) {
+        Long viewerId = (userDto != null) ? userDto.getId() : null;
+        List<FollowListDto> list = followService.getFollowings(userId, viewerId);
+        return ResponseEntity.ok(list);
     }
 }
