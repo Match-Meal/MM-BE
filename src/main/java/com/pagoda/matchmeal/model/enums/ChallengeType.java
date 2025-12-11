9b1b4595ee919584f774817f7abe0153800f5a76
seho1278 <seho1278@naver.com>
2025-12-12 00:25:06 +0900

    feat: 챌린지 타입 enum으로 정의

---
+++ src/main/java/com/pagoda/matchmeal/model/enums/ChallengeType.java
@@ -0,0 +1,12 @@
+package com.pagoda.matchmeal.model.enums;
+
+public enum ChallengeType {
+    // 습관형 (예 : 연속 기록하기)
+    RECORD_FREQUENCY,
+
+    // 영양형 (예 : 500kcal 미만 식사)
+    CALORIE_LIMIT,
+
+    // 시간형 (예 : 아침 챙겨 먹기 06:00 ~ 09:00)
+    TIME_RANGE
+}
