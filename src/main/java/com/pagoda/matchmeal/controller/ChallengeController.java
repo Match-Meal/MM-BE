f5968125a8add0a2e7d4e23a65acdb9da44ef725
seho1278 <seho1278@naver.com>
2025-12-14 06:07:32 +0900

    feat: 챌린지 ì초대정보 저장 entity 생성

--- src/main/java/com/pagoda/matchmeal/controller/ChallengeController.java
+++ src/main/java/com/pagoda/matchmeal/controller/ChallengeController.java
@@ -8,9 +8,7 @@ import com.pagoda.matchmeal.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,13 +19,29 @@ public class ChallengeController {
 
     private final ChallengeService challengeService;
 
-    // 챌린지 목록 조회
-//    @GetMapping
-//    public ResponseEntity<CommonResponse<List<ChallengeResponseDto>>> getChallenges(
-//            @AuthenticationPrincipal UserDto user) {
-//        return ResponseEntity.ok(
-//                ApiResponseUtil.success(challengeService.get)
-//        )
-//    }
+    /**
+     * 전체 챌린지 조회
+     * @param user
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<CommonResponse<List<ChallengeResponseDto>>> getChallenges(
+            @AuthenticationPrincipal UserDto user) {
+        return ResponseEntity.ok(
+                ApiResponseUtil.success(challengeService.getAllChallenges(user.getId()))
+        );
+    }
+
+    @PostMapping("/{challengeId}/join")
+    public ResponseEntity<CommonResponse<Void>> joinChallenge(
+            @AuthenticationPrincipal UserDto user,
+            @PathVariable Long challengeId) {
+
+        challengeService.joinChallenge(user.getId(), challengeId);
+        return ResponseEntity.ok(ApiResponseUtil.success());
+    }
+
+
+
 
 }
--- src/main/java/com/pagoda/matchmeal/model/entity/Challenge.java
+++ src/main/java/com/pagoda/matchmeal/model/entity/Challenge.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Challenge {
     private Long challengeId;
+    private Long ownerId; // 생성자 id
     private String title;
     private String description;
     private ChallengeType type;
@@ -27,6 +28,11 @@ public class Challenge {
     
     // 성공해야하는 횟수
     private int goalCount;
+    
+    private int maxParticipants; // 최대 인원
+    private boolean isPublic; // 공개 여부(true: 공개, false: 비공개)
+    private String invitationCode; // 참여 코드 (난수 문자열)
+    private int currentHeadCount; // 현재 참여 인원
 
     private LocalDateTime createdAt;
 }
