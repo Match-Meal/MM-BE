360053a9b34634c795fab85c4a892d351172d6db
seho1278 <seho1278@naver.com>
2025-12-14 06:40:06 +0900

    feat: 챌린지 초대관련 mapper 작성

---
+++ src/main/java/com/pagoda/matchmeal/model/dto/ChallengeSearchCondition.java
@@ -0,0 +1,2 @@
+package com.pagoda.matchmeal.model.dto;public class ChallengeSearchCondition {
+}
---
+++ src/main/java/com/pagoda/matchmeal/model/dto/response/ChallengeCreateResponseDto.java
@@ -0,0 +1,2 @@
+package com.pagoda.matchmeal.model.dto.response;public class ChallengeCreateResponseDto {
+}
